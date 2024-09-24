e24e2471e18ac6dfcc83a3b78e1668c820f77d0c
AnkurDutta21 <daxxz2212@gmail.com>
2024-09-24 02:23:20 -0700

    added auth middleware

--- src/routes/email.js
+++ src/routes/email.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const { subscribeNewsletter, getLeadData, freeDownloadsRequest, freeDownloadsVerifyOtp } = require('../controllers/emailController')
-
+const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router()
 
 router.post('/subscribeToNewsletter',subscribeNewsletter)
 router.post('/freeDownloadsReq',freeDownloadsRequest)
 router.post('/freeDownloadsVerify',freeDownloadsVerifyOtp)
-router.get('/getLeadData',getLeadData)
+router.get('/getLeadData',authMiddleware,getLeadData)
 
 
 module.exports = router; 
