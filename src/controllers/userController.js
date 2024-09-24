20cbecfda1cccae5b59cd1ab8a583f7a3c763427
AnkurDutta21 <daxxz2212@gmail.com>
2024-09-24 04:28:05 -0700

    refactored : userauth and refresh token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,56 +2,61 @@ const messageHelper = require("../utils/messageHelper");
 const { errorResponse, successResponse } = require("../utils/responseHelper");
 const asyncHandler = require("../utils/asyncHandler");
 const userHelper = require("../helpers/userHelper");
+const Auth = require("../utils/auth");
 
 module.exports = {
   registerController: asyncHandler(async (req, res) => {
-    const { phoneNumber  } = req.body;
+    const { phoneNumber } = req.body;
 
-    // Validate required fields
-    if (!phoneNumber ) {
-      errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
+    if (!phoneNumber) {
+      return errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
     }
 
     const result = await userHelper.register(req.body);
     if (!result) {
-      errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
+      return errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
     }
-    successResponse(res, 200, messageHelper.OTP_SENT_SUCCESSFULLY,result);
+
+    const accessToken = Auth.createAccessToken({ phoneNumber }, process.env.ACCESS_TOKEN_SECRET);
+    const refreshToken = Auth.createRefreshToken({ phoneNumber }, process.env.REFRESH_TOKEN_SECRET);
+
+    return successResponse(res, 200, messageHelper.OTP_SENT_SUCCESSFULLY, { result, accessToken, refreshToken });
   }),
+
   loginController: asyncHandler(async (req, res) => {
     const { phoneNumber } = req.body;
-    const token = req.headers.authorization?.split(" ")[1];
-    console.log("token", token);
 
-    if (!phoneNumber ) {
-      errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
+    if (!phoneNumber) {
+      return errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
     }
 
     const result = await userHelper.login({ phoneNumber });
     if (!result) {
-      errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
+      return errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
     }
-    console.log("Login result", result);
-    successResponse(res, 200, messageHelper.OTP_SENT_SUCCESSFULLY,result);
+
+    const accessToken = Auth.createAccessToken({ phoneNumber }, process.env.ACCESS_TOKEN_SECRET);
+    const refreshToken = Auth.createRefreshToken({ phoneNumber }, process.env.REFRESH_TOKEN_SECRET);
+
+    return successResponse(res, 200, messageHelper.OTP_SENT_SUCCESSFULLY, { result, accessToken, refreshToken });
   }),
 
   verifyUserRegistrationController: asyncHandler(async (req, res) => {
     const { otp, phoneNumber } = req.body;
-    console.log(otp,'popooppo')
+
     if (!otp) {
-      errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
+      return errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
     }
+
     const result = await userHelper.verifyUserRegistration(otp, phoneNumber);
     if (!result) {
-      errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
+      return errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
     }
-    console.log("otp varification result", result);
-    successResponse(
-      res,
-      200,
-      messageHelper.SUCCESSFULLY_REGISTERED_USER,
-      result
-    );
+
+    const accessToken = Auth.createAccessToken({ phoneNumber }, process.env.ACCESS_TOKEN_SECRET);
+    const refreshToken = Auth.createRefreshToken({ phoneNumber }, process.env.REFRESH_TOKEN_SECRET);
+
+    return successResponse(res, 200, messageHelper.SUCCESSFULLY_REGISTERED_USER, { result, accessToken, refreshToken });
   }),
 
   verifyUserLoginController: asyncHandler(async (req, res) => {
@@ -59,30 +64,45 @@ module.exports = {
     const token = req.headers.authorization?.split(" ")[1];
 
     if (!otp) {
-      errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
+      return errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
     }
+
     const result = await userHelper.verifyUserLogin(phoneNumber, otp, token);
     if (!result) {
-      errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
+      return errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
     }
-    console.log("otp varification result", result);
-    successResponse(res, 200, messageHelper.SUCCESSFULLY_LOGGED_USER, result);
+
+    const accessToken = Auth.createAccessToken({ phoneNumber }, process.env.ACCESS_TOKEN_SECRET);
+    const refreshToken = Auth.createRefreshToken({ phoneNumber }, process.env.REFRESH_TOKEN_SECRET);
+
+    return successResponse(res, 200, messageHelper.SUCCESSFULLY_LOGGED_USER, { result, accessToken, refreshToken });
   }),
 
   logoutController: asyncHandler(async (req, res) => {
-    const {phoneNumber} = req.body
     const token = req.headers.authorization?.split(" ")[1];
-    console.log(token)
     if (!token) {
-      errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
+      return errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
     }
 
-    const result = await userHelper.logout(token,phoneNumber);
-    console.log(result)
+    const result = await userHelper.logout(token);
     if (!result) {
-      errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
+      return errorResponse(res, 500, messageHelper.INTERNAL_SERVER_ERROR);
     }
-    console.log("logoutResult", result);
-    successResponse(res, 200, messageHelper.SUCCESSFULLY_LOGOUT_USER, result);
+    return successResponse(res, 200, messageHelper.SUCCESSFULLY_LOGOUT_USER, result);
+  }),
+
+  refreshTokenController: asyncHandler(async (req, res) => {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      return errorResponse(res, 400, messageHelper.MISSING_REQUIRED_FIELDS);
+    }
+
+    try {
+      const decoded = Auth.verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+      const accessToken = Auth.createAccessToken({ phoneNumber: decoded.phoneNumber }, process.env.ACCESS_TOKEN_SECRET);
+      return successResponse(res, 200, messageHelper.TOKEN_REFRESHED, { accessToken });
+    } catch (error) {
+      return errorResponse(res, 401, messageHelper.INVALID_REFRESH_TOKEN);
+    }
   }),
 };
