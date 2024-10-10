4add51076d738be4f37d69a76958583a943a622a
AnkurDutta21 <daxxz2212@gmail.com>
2024-10-10 05:03:23 -0700

    updted acl for file upload

--- src/services/FileUpload.js
+++ src/services/FileUpload.js
@@ -19,12 +19,14 @@ async function uploadFile(file,userId, mimetype, originalname) {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: key,
     Body: fileBuffer,
-    ContentType:mimetype
+    ContentType: mimetype,
+    ACL: 'public-read',
   };
 
   try {
     const result = await s3.upload(params).promise();
     console.log('File uploaded successfully:', result.Location);
+    console.log(result)
     return result.Location;
   } catch (error) {
     console.error('Error uploading file:', error);
