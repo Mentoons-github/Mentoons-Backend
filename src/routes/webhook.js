e123628b4f1d5be17f4b815f89de91888d11a7d6
Dheeraj Sharma <at60sharwan@gmail.com>
2024-10-15 15:24:13 +0530

    Authentication fix

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const { clerkWebhookConroller } = require("../controllers/clerk-webhook");
 const router = express.Router();
+const { clerkMiddleware } = require("@clerk/express");
 
-router.post("/clerk",clerkWebhookConroller)
+router.post(
+  "/clerk",
+  clerkMiddleware({ secretKey: process.env.CLERK_WEBHOOK_SECRET }),
+  clerkWebhookConroller
+);
 
 module.exports = router;
