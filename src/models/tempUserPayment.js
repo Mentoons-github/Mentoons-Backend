1bcae4a26495fc8545050b4d7e7caa2670f97bc8
Devanps <devanps212@gmail.com>
2025-03-29 12:08:59 +0530

    temporary storage added for user and order for ccavenue response check

---
+++ src/models/tempUserPayment.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const temporaryUserSchema = new mongoose.Schema({
+  orderId: { type: String, required: true, ref: "Order", unique: true },
+  userId: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, expires: 900 },
+});
+
+const TemporaryUser = mongoose.model("TemporaryUser", temporaryUserSchema);
+
+module.exports = TemporaryUser;
