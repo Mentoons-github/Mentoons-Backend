75a8a724d4941f6c4d9b13ed97ab70a5fb1a1cdd
Devanps <devanps212@gmail.com>
2025-03-31 10:53:17 +0530

    middleware added and subscription adde

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const paymentController = require("../controllers/paymentController.js");
 const ccavResponseHandler = require("../controllers/ccavResponseHandler.js");
-const conditionalAuth = require("../middlewares/auth.middleware.js")
 
 // Route to initiate payment
-router.post("/initiate", conditionalAuth, paymentController.initiatePayment);
+router.post("/initiate", paymentController.initiatePayment);
 
 // Route to handle CCAvenue response
 router.post("/ccavenue-response", ccavResponseHandler.postRes);
