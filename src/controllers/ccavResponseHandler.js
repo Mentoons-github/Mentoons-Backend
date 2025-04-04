479dbec6fb2bb4c847fcd40b7267dc6c00750e5c
Devanps <devanps212@gmail.com>
2025-04-04 13:01:52 +0530

    ccavenue response modified

--- src/controllers/ccavResponseHandler.js
+++ src/controllers/ccavResponseHandler.js
@@ -78,7 +78,7 @@ const postRes = async (request, response) => {
         await order.populate("products");
         await order.populate("user");
 
-        console.log("Order update after populating result:", order);
+        console.log("Order update after populating result:", order.products);
 
         console.log(
           `Order ${responseObject.order_id} updated with status: ${orderStatus}`
