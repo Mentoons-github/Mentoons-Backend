4b6fa7a7d1e943929b1a66405c9742b86ddf0f61
Devanps <devanps212@gmail.com>
2025-04-09 13:43:29 +0530

    session contoller created for getting al sesions

--- src/controllers/session.js
+++ src/controllers/session.js
@@ -1,10 +1,29 @@
-const addSession = async () => {
+const SessionModel = require("../models/session");
+const User = require("../models/user");
+
+const getUserSession = async (req, res) => {
   try {
-    const sesionData = req.body;
+    const userClerkId = req.user.id;
+
+    const user = await User.findOne({ clerkId: userClerkId });
+
+    const existingSession = await SessionModel.findOne({ user: user._id });
+    if (existingSession.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No session found",
+      });
+    }
+
+    return res.status(200).json({ success: true, session: existingSession });
   } catch (error) {
     res.status(400).json({
       success: false,
       message: error.message,
     });
   }
+};
+
+module.exports = {
+  getUserSession,
 };
