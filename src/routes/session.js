091f8c301a86793fcd939d11d31107ffa1fc4988
Devanps <devanps212@gmail.com>
2025-04-11 15:51:00 +0530

    session postponed completed

--- src/routes/session.js
+++ src/routes/session.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { getUserSession } = require("../controllers/session");
+const { getUserSession, availabiltyCheck } = require("../controllers/session");
 const { conditionalAuth } = require("../middlewares/auth.middleware");
 
 const sessionRoute = express.Router();
 
-sessionRoute.get("/getbookings", conditionalAuth, getUserSession);
+sessionRoute.use(conditionalAuth);
+
+sessionRoute.get("/getbookings", getUserSession);
+sessionRoute.get("/availability", availabiltyCheck);
 
 module.exports = sessionRoute;
