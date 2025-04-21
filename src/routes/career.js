8e692190a76eb6fc87413510c7b017d5ede80d32
Dheeraj Sharma <at60sharwan@gmail.com>
2025-04-21 19:28:55 +0530

    Create and endpoint for fetching job application by id

--- src/routes/career.js
+++ src/routes/career.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {addJob, getJobs, getJobById, applyJob, editJob, deleteJob, getAppliedJobs} = require('../controllers/career');
+const {
+  addJob,
+  getJobs,
+  getJobById,
+  applyJob,
+  editJob,
+  deleteJob,
+  getAppliedJobs,
+  getAppliedJobById,
+} = require("../controllers/career");
 
-router.route('/jobs').post(addJob).get(getJobs);
-router.route('/jobs/:id').get(getJobById).put(editJob).delete(deleteJob);
-router.route('/jobs/apply/:id').post(applyJob);
-router.route('/applied').get(getAppliedJobs);
+router.route("/jobs").post(addJob).get(getJobs);
+router.route("/jobs/:id").get(getJobById).put(editJob).delete(deleteJob);
+router.route("/jobs/apply/:id").post(applyJob);
+router.route("/applied").get(getAppliedJobs);
+router.route("/applied/:id").get(getAppliedJobById);
 module.exports = router;
