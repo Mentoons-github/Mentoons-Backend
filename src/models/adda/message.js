e7a01f8029508f390f839b5f1a8b88af9e5dd8f4
Devanps <devanps212@gmail.com>
2025-06-20 10:15:42 +0530

    live notification completed

--- src/models/adda/message.js
+++ src/models/adda/message.js
@@ -18,6 +18,14 @@ const messageSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    isRead: {
+      type: Boolean,
+      required: false,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+    },
   },
   {
     timestamps: true,
