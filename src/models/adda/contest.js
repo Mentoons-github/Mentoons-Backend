7327a2847e18415c9dc53581e878e2668a00c389
Devanps <devanps212@gmail.com>
2025-11-21 18:03:00 +0530

    extra field added on the contest mpodel

--- src/models/adda/contest.js
+++ src/models/adda/contest.js
@@ -2,10 +2,36 @@ const mongoose = require("mongoose");
 
 const ContestSchema = new mongoose.Schema(
   {
-    name: String,
-    age: Number,
-    mobile: Number,
-    images: [String],
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 5,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "social media logos",
+        "accessories",
+        "daily stationary items",
+        "gadgets",
+        "musicians",
+        "animated movie characters",
+      ],
+    },
   },
   { timestamps: true }
 );
