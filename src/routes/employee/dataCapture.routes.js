f53c3ca95a435ab78552c71b5eeea21d85a8ef68
MohammedJasim001 <jasimat001@gmail.com>
2026-02-04 15:16:07 +0530

    add scoring system in workshop

--- src/routes/employee/dataCapture.routes.js
+++ src/routes/employee/dataCapture.routes.js
@@ -5,6 +5,7 @@ const {
   fetchSingleDataCaptureDetails,
   addReviewOnDataCapture,
   editDataCapture,
+  addScoringSystem,
 } = require("../../controllers/employee/dataCaptureController.js");
 const { verifyRole } = require("../../middlewares/admin/adminAuth.js");
 
@@ -16,12 +17,17 @@ router.get("/fetch", verifyRole(["EMPLOYEE"]), fetchDataCaptureDetails);
 router.get(
   "/fetch-single/:dataCaptureId",
   verifyRole(["EMPLOYEE"]),
-  fetchSingleDataCaptureDetails
+  fetchSingleDataCaptureDetails,
 );
 router.put(
   "/add-review/:dataCaptureId",
   verifyRole(["EMPLOYEE"]),
-  addReviewOnDataCapture
+  addReviewOnDataCapture,
+);
+router.put(
+  "/add-score/:dataCaptureId",
+  verifyRole(["EMPLOYEE"]),
+  addScoringSystem,
 );
 
 module.exports = router;
