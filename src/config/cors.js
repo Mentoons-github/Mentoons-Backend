3069fb494a18b613bfefd8ecc8c895edb14a5d13
Devan P S <devanps212@gmail.com>
2026-03-10 19:31:16 +0530

    vercel link added

--- src/config/cors.js
+++ src/config/cors.js
@@ -11,6 +11,7 @@ module.exports = {
     "https://mentoons-website-4wmixkbtq-mentoons-projects.vercel.app",
     "https://mentoons-website-981v3d0hh-mentoons-projects.vercel.app",
     "https://mentoons-website-hl6g0xz8u-mentoons-projects.vercel.app",
+    "https://mentoons-website-hk5cny5sg-mentoons-projects.vercel.app",
     "https://mentoons-website-nine.vercel.app",
     "https://mentormahesh.com",
   ],
